a381674330e8381c8a4f4b1e609eb7aa373442ec
corncode8 <myeongcheol8@naver.com>
2024-06-16 20:57:39 +0900

    Initial commit

---
+++ src/main/java/kfriday/kfriday_homework/KfridayHomeworkApplication.java
@@ -0,0 +1,13 @@
+package kfriday.kfriday_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfridayHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfridayHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kfriday/kfriday_homework/KfridayHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kfriday.kfriday_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfridayHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
